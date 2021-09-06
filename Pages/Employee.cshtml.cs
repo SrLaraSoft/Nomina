bc11147a23ded27a3c48bb59de2fe3c60a1d0ecc
SrLaraSoft <58270947+SrLaraSoft@users.noreply.github.com>
2021-09-05 23:17:34 -0400

    Refresh

--- Pages/Employee.cshtml.cs
+++ Pages/Employee.cshtml.cs
@@ -22,22 +22,12 @@ namespace Nomina.Pages
                 new Employee(){ nombre = "Manuel", apellido = "Garcia Rodriguez", cargo = "Diseñador", sueldo = 20000},
                 new Employee(){ nombre = "Katerin", apellido = "Perez", cargo = "Administrador", sueldo = 269640},
                 new Employee(){ nombre = "Rossy", apellido = "Lora", cargo = "Asistente", sueldo = 1},
-                new Employee(){ nombre = "Juan", apellido = "Lopez", cargo = "Programador", sueldo = 30000},
-                new Employee(){ nombre = "Luis", apellido = "Ruiz", cargo = "Desarrollador", sueldo = 80000},
+                new Employee(){ nombre = "Juan", apellido = "Lopez", cargo = "Programador", sueldo = 28500},
+                new Employee(){ nombre = "Luis", apellido = "Ruiz", cargo = "Desarrollador", sueldo = 21000},
                 new Employee(){ nombre = "Manuel", apellido = "Garcia", cargo = "Diseñador", sueldo = 55000},
                 new Employee(){ nombre = "Katerin", apellido = "Perez", cargo = "Administrador", sueldo = 70000},
                 new Employee(){ nombre = "Rossy", apellido = "Lora", cargo = "Asistente", sueldo = 60000},
-                new Employee(){ nombre = "Juan", apellido = "Lopez", cargo = "Programador", sueldo = 50000},
-                new Employee(){ nombre = "Luis", apellido = "Ruiz", cargo = "Desarrollador", sueldo = 60000},
-                new Employee(){ nombre = "Manuel", apellido = "Garcia", cargo = "Diseñador", sueldo = 45000},
-                new Employee(){ nombre = "Katerin", apellido = "Perez", cargo = "Administrador", sueldo = 80000},
-                new Employee(){ nombre = "Rossy", apellido = "Lora", cargo = "Asistente", sueldo = 300000},
-                new Employee(){ nombre = "Juan", apellido = "Lopez", cargo = "Programador", sueldo = 70000},
-                new Employee(){ nombre = "Luis", apellido = "Ruiz", cargo = "Desarrollador", sueldo = 80000},
-                new Employee(){ nombre = "Manuel", apellido = "Garcia", cargo = "Diseñador", sueldo = 55000},
-                new Employee(){ nombre = "Katerin", apellido = "Perez", cargo = "Administrador", sueldo = 700000},
-                new Employee(){ nombre = "Rossy", apellido = "Lora", cargo = "Asistente", sueldo = 60000}
-            };
+                };
 
         }
     }
